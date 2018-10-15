06e1daf9ea017e40366ea3403dc4967e38fe7223
yahmez00 <jdofficer1@gmail.com>
2018-10-15 22:01:47 +1100

    Can now clone a component (localised to current Canvas for now).

---
+++ resources/assets/js/components/topbar/CloneComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <top-bar-control @click.native="copyComponentStyle" variant="outline-primary" icon="clone" tooltip="Clone Component"></top-bar-control>
+</template>
+
+<script>
+import TopBarControl from './TopBarControl';
+
+export default {
+    name: "CopyStyle",
+
+    components: { TopBarControl },
+
+    methods: {
+        copyComponentStyle() {
+            console.log('copied');
+        }
+    }
+}
+</script>
