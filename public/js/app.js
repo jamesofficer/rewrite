24790ed375cdd82753412ff8d18c39730ec16b4b
James <jdofficer1@gmail.com>
2019-01-19 15:34:57 +1100

    Stylesheet string export now functional.

--- public/js/app.js
+++ public/js/app.js
@@ -57193,6 +57193,10 @@ __WEBPACK_IMPORTED_MODULE_0__components_Icon_vue___default.a.register({"brands/p
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return generateHTML; });
+var stylesheet = '';
+
+var propertiesToIgnore = ['componentAlignment', 'columnWidth', 'columnOffset', 'content', 'fontWeights', 'src'];
+
 var deviceSizeToPx = {
     sm: '576px',
     md: '768px',
@@ -57200,33 +57204,46 @@ var deviceSizeToPx = {
     xl: '1200px'
 };
 
-var camelCaseToDash = function camelCaseToDash(str) {
-    return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
-};
-
 var generateHTML = function generateHTML(state) {
     var canvases = state.canvases;
 
-    canvases.forEach(function (canvas) {
-        state.deviceSizes.forEach(function (size) {
-            convertElementToCSS(canvas[size], canvas.identifier, deviceSizeToPx[size]);
+    state.deviceSizes.forEach(function (size) {
+        stylesheet += '@media (min-width: ' + deviceSizeToPx[size] + ') { \n';
+
+        canvases.forEach(function (canvas) {
+            convertElementToCSS(canvas[size], canvas.identifier);
+
+            canvas.rows.forEach(function (row) {
+                convertElementToCSS(row[size], row.identifier);
+
+                row.columns.forEach(function (column) {
+                    convertElementToCSS(column[size], column.identifier);
+
+                    column.components.forEach(function (component) {
+                        convertElementToCSS(component[size], component.identifier);
+                    });
+                });
+            });
         });
+
+        stylesheet += '} \n \n';
     });
+
+    console.log(stylesheet);
 };
 
-var convertElementToCSS = function convertElementToCSS(element, elementIdentifier, mediaQueryWidth) {
+var convertElementToCSS = function convertElementToCSS(element, elementIdentifier) {
     var properties = Object.keys(element);
-    var elementCSS = '@media (min-width: ' + mediaQueryWidth + ') { \n';
-
-    elementCSS += '.' + elementIdentifier + ' { \n';
+    var elementCSS = '\t.' + elementIdentifier + ' { \n';
 
     properties.forEach(function (property) {
-        elementCSS += getPropertyCSS(element, property) + '\n';
+        if (!propertiesToIgnore.includes(property)) {
+            elementCSS += '\t \t' + getPropertyCSS(element, property) + '\n';
+        }
     });
 
-    elementCSS += '} \n } \n';
-
-    console.log(elementCSS);
+    elementCSS += '\t } \n \n';
+    stylesheet += elementCSS;
 };
 
 /**
@@ -57237,7 +57254,11 @@ var convertElementToCSS = function convertElementToCSS(element, elementIdentifie
 var getPropertyCSS = function getPropertyCSS(element, propertyName) {
     var funcName = 'get' + propertyName.charAt(0).toUpperCase() + propertyName.slice(1) + 'CSS';
 
-    return eval(funcName + '(element[propertyName])');
+    var result = eval(funcName + '(element[propertyName])');
+
+    // console.log(result);
+
+    return result;
 };
 
 function getBackgroundSizeCSS(size) {
@@ -57252,8 +57273,56 @@ function getBackgroundColorCSS(colors) {
     return 'background-color: rgba(' + colors.r + ', ' + colors.g + ', ' + colors.b + ', ' + colors.a + ');';
 }
 
+function getMarginCSS(margin) {
+    return 'margin: ' + margin.top + 'px ' + margin.right + 'px ' + margin.bottom + 'px ' + margin.left + 'px;';
+}
+
 function getPaddingCSS(padding) {
     return 'padding: ' + padding.top + 'px ' + padding.right + 'px ' + padding.bottom + 'px ' + padding.left + 'px;';
+}
+
+function getBorderCSS(border) {
+    return 'border: ' + border.top + 'px ' + border.right + 'px ' + border.bottom + 'px ' + border.left + 'px ' + border.style + ' rgba(' + border.color.r + ', ' + border.color.g + ', ' + border.color.b + ', ' + border.color.a + ');';
+}
+
+function getBoxShadowCSS(shadow) {
+    return 'box-shadow: ' + shadow.offsetX + 'px ' + shadow.offsetY + 'px ' + shadow.blurRadius + 'px rgba(' + shadow.color.r + ', ' + shadow.color.g + ', ' + shadow.color.b + ', ' + shadow.color.a + ');';
+}
+
+function getTextShadowCSS(shadow) {
+    return 'text-shadow: ' + shadow.offsetX + 'px ' + shadow.offsetY + 'px ' + shadow.blurRadius + 'px rgba(' + shadow.color.r + ', ' + shadow.color.g + ', ' + shadow.color.b + ', ' + shadow.color.a + ');';
+}
+
+function getWidthCSS(width) {
+    return 'width: ' + width + '%;';
+}
+
+function getFontFamilyCSS(fontFamily) {
+    return 'font-family: ' + fontFamily + ', Helvetica, Arial, sans-serif;';
+}
+
+function getFontWeightCSS(fontWeight) {
+    return 'font-weight: ' + fontWeight + ';';
+}
+
+function getFontSizeCSS(fontSize) {
+    return 'font-size: ' + fontSize + ';';
+}
+
+function getLineHeightCSS(lineHeight) {
+    return 'line-height: ' + lineHeight + ';';
+}
+
+function getLetterSpacingCSS(letterSpacing) {
+    return 'letter-spacing: ' + letterSpacing + 'px;';
+}
+
+function getTextAlignCSS(textAlign) {
+    return 'text-align: ' + textAlign + ';';
+}
+
+function getTextColorCSS(color) {
+    return 'color: rgba(' + color.r + ', ' + color.g + ', ' + color.b + ', ' + color.a + ');';
 }
 
 /***/ })
--- resources/assets/js/store/generate.js
+++ resources/assets/js/store/generate.js
@@ -1,3 +1,9 @@
+let stylesheet = '';
+
+const propertiesToIgnore = [
+    'componentAlignment', 'columnWidth', 'columnOffset', 'content', 'fontWeights', 'src',
+];
+
 const deviceSizeToPx = {
     sm: '576px',
     md: '768px',
@@ -5,33 +11,46 @@ const deviceSizeToPx = {
     xl: '1200px',
 };
 
-const camelCaseToDash = (str) => {
-    return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
-}
-
 export const generateHTML = (state) => {
     const canvases = state.canvases;
 
-    canvases.forEach(function (canvas) {
-        state.deviceSizes.forEach(function (size) {
-            convertElementToCSS(canvas[size], canvas.identifier, deviceSizeToPx[size]);
+    state.deviceSizes.forEach(function (size) {
+        stylesheet += '@media (min-width: ' + deviceSizeToPx[size] + ') { \n';
+
+        canvases.forEach(function (canvas) {
+            convertElementToCSS(canvas[size], canvas.identifier);
+
+            canvas.rows.forEach(function (row) {
+                convertElementToCSS(row[size], row.identifier);
+
+                row.columns.forEach(function (column) {
+                    convertElementToCSS(column[size], column.identifier);
+
+                    column.components.forEach(function (component) {
+                        convertElementToCSS(component[size], component.identifier);
+                    });
+                });
+            });
         });
+
+        stylesheet += '} \n \n';
     });
+
+    console.log(stylesheet);
 }
 
-const convertElementToCSS = (element, elementIdentifier, mediaQueryWidth) => {
+const convertElementToCSS = (element, elementIdentifier) => {
     const properties = Object.keys(element);
-    let elementCSS   = '@media (min-width: ' + mediaQueryWidth + ') { \n';
-
-    elementCSS += '.' + elementIdentifier + ' { \n'
+    let elementCSS   = '\t.' + elementIdentifier + ' { \n'
 
     properties.forEach(function (property) {
-        elementCSS += getPropertyCSS(element, property) + '\n';
+        if (! propertiesToIgnore.includes(property)) {
+            elementCSS += '\t \t' + getPropertyCSS(element, property) + '\n';
+        }
     });
 
-    elementCSS += '} \n } \n';
-
-    console.log(elementCSS);
+    elementCSS += '\t } \n \n';
+    stylesheet += elementCSS;
 }
 
 /**
@@ -42,7 +61,11 @@ const convertElementToCSS = (element, elementIdentifier, mediaQueryWidth) => {
 const getPropertyCSS = (element, propertyName) => {
     const funcName  = 'get' + propertyName.charAt(0).toUpperCase() + propertyName.slice(1) + 'CSS';
 
-    return eval(`${funcName}(element[propertyName])`);
+    const result = eval(`${funcName}(element[propertyName])`);
+
+    // console.log(result);
+
+    return result;
 }
 
 function getBackgroundSizeCSS (size) {
@@ -57,7 +80,54 @@ function getBackgroundColorCSS (colors) {
     return `background-color: rgba(${colors.r}, ${colors.g}, ${colors.b}, ${colors.a});`;
 }
 
+function getMarginCSS (margin) {
+    return `margin: ${margin.top}px ${margin.right}px ${margin.bottom}px ${margin.left}px;`;
+}
+
 function getPaddingCSS (padding) {
     return `padding: ${padding.top}px ${padding.right}px ${padding.bottom}px ${padding.left}px;`;
 }
 
+function getBorderCSS (border) {
+    return `border: ${border.top}px ${border.right}px ${border.bottom}px ${border.left}px ${border.style} rgba(${border.color.r}, ${border.color.g}, ${border.color.b}, ${border.color.a});`;
+}
+
+function getBoxShadowCSS (shadow) {
+    return `box-shadow: ${shadow.offsetX}px ${shadow.offsetY}px ${shadow.blurRadius}px rgba(${shadow.color.r}, ${shadow.color.g}, ${shadow.color.b}, ${shadow.color.a});`;
+}
+
+function getTextShadowCSS (shadow) {
+    return `text-shadow: ${shadow.offsetX}px ${shadow.offsetY}px ${shadow.blurRadius}px rgba(${shadow.color.r}, ${shadow.color.g}, ${shadow.color.b}, ${shadow.color.a});`;
+}
+
+function getWidthCSS (width) {
+    return `width: ${width}%;`;
+}
+
+function getFontFamilyCSS (fontFamily) {
+    return `font-family: ${fontFamily}, Helvetica, Arial, sans-serif;`;
+}
+
+function getFontWeightCSS (fontWeight) {
+    return `font-weight: ${fontWeight};`;
+}
+
+function getFontSizeCSS (fontSize) {
+    return `font-size: ${fontSize};`;
+}
+
+function getLineHeightCSS (lineHeight) {
+    return `line-height: ${lineHeight};`;
+}
+
+function getLetterSpacingCSS (letterSpacing) {
+    return `letter-spacing: ${letterSpacing}px;`;
+}
+
+function getTextAlignCSS (textAlign) {
+    return `text-align: ${textAlign};`;
+}
+
+function getTextColorCSS (color) {
+    return `color: rgba(${color.r}, ${color.g}, ${color.b}, ${color.a});`;
+}
